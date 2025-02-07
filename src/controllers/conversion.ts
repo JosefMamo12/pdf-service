fe88edbe8ad0feeae3669df248a0a38c5714811c
JosefTheKing12 <yosimami50@gmail.com>
2025-02-07 13:14:05 +0200

    change to local developing

--- src/controllers/conversion.ts
+++ src/controllers/conversion.ts
@@ -4,16 +4,17 @@ import { main } from "../helpers/generator";
 const router = Router();
 
 router.post("/", async (req, res) => {
-	const { html } = req.body;
-	console.log("HTML", html);
+  const { html } = req.body;
+  console.log("HTML", html);
 
-	if (!html) {
-		res.status(400).send("HTML is required");
-		return;
-	}
-	const pdf = await main(html);
-	res.contentType("application/pdf");
-	res.send(pdf);
+  if (!html) {
+    res.status(400).send("HTML is required");
+    return;
+  }
+  const pdf = await main(html);
+  res.setHeader("Content-Disposition", "attachment; filename=document.pdf");
+  res.contentType("application/pdf");
+  res.send(pdf);
 });
 
 export default router;
